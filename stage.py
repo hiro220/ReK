ba9a6ce15e00b44c09420413ad1a7f4094251b7d
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-02-27 17:32:32 +0900

    コメントの追加

--- stage.py
+++ stage.py
@@ -20,9 +20,9 @@ class Stage:
         self.width, _ = self.rect.midright      # 背景画像のサイズ、_は使わない部分の値
         self.speed = 1                          # 背景の移動速度
         
-        self.initGroup()
+        self.initGroup()                        # グループを初期化する
 
-        self.readStage(filename)
+        self.readStage(filename)                # ステージ情報を読み込む
 
         self.player = PlayerMachine(PLAYER_X, PLAYER_Y, self.cpus)    # プレイヤーのマシンを生成する
 
@@ -48,20 +48,21 @@ class Stage:
 
     def process(self):
         # 1フレームごとの処理
-        self.createCpu()
-        self.moveStage()
-        self.player.move(HEIGHT, WIDTH)  # 入力に応じてプレイヤーの機体を動かす
-        self.group.update()         # groupに割り当てられたすべてのスプライトを更新する
+        self.createCpu()                    # cpuの生成を行う
+        self.moveStage()                    # ステージを動かす
+        self.player.move(HEIGHT, WIDTH)     # 入力に応じてプレイヤーの機体を動かす
+        self.group.update()                 # groupに割り当てられたすべてのスプライトを更新する
         for event in pygame.event.get():
-            if event.type == QUIT:      # 「閉じるボタン」を押したとき
+            if event.type == QUIT:          # 「閉じるボタン」を押したとき
                 return EXIT
-            if event.type == KEYDOWN:   # キー入力があった時
+            if event.type == KEYDOWN:       # キー入力があった時
                 self.player.shoot(event.key)    # 押したキーに応じて弾を発射する
         return CONTINUE
 
     def moveStage(self):
-        if self.x - 1 >= self.size:
+        if self.x - 1 >= self.size:         # 画面がステージサイズ分移動しているなら早期リターン
             return
+
         self.x += self.speed                # ステージの位置を移動させる
         if self.x - 1 >= self.width:        # 画像が端までいったとき、背景画像と反転画像を入れ替えて、位置を初期化する
             self.x = 0
@@ -78,29 +79,33 @@ class Stage:
         pygame.display.update()             # 画面を更新する
 
     def readStage(self, file):
-        with open(file, 'r', encoding="utf-8") as fp:
-            key = 0
-            self.dic = {}
-            for line in fp.readlines():
-                line = line.strip('\n').split()
-                if len(line) == 1:
-                    key = int(line[0])
-                    self.dic[key] = []
+        """引数に指定したテキストファイルからステージ情報を読み込み、cpu情報をx座標がkeyとなる辞書型に格納する。（同様にアイテムの読み込みもできるはず）
+        画面サイズがテキストファイルに記述されていない場合、初期値として0が代入される"""
+        with open(file, 'r', encoding="utf-8") as fp:   # ファイルを読み取り専用で開く
+            self.size = key = 0                         # 画面サイズと、辞書のkeyを0に初期化する
+            self.dic = {}                               # 辞書を定義する
+            for line in fp.readlines():                 # ファイルを一行ごとに読み取り、変数lineに文字列として格納する
+                line = line.strip('\n').split()         # 改行コード'\n'を取り除き、タブ区切りでリストに分割する
+                if len(line) == 1:                      # リストの要素数が1のとき、keyとなるx座標が記述されている
+                    key = int(line[0])                  # 文字列をintに変換
+                    self.dic[key] = []                  # 辞書にkeyを追加し、その値をリストとして初期化しておく
                     continue
-                if len(line) == 2:
-                    if line[0] == 'size':
+                if len(line) == 2:                      # リストの要素数が2のとき、ステージサイズかcpu情報が記述されている(ここにアイテム追加も可)
+                    if line[0] == 'size':               # 要素の一つ目がsizeのとき、二つ目の要素にステージサイズが記述されている
                         self.size = int(line[1])
-                    else:
-                        self.dic[key].append([line[0], line[1]])
+                    else:                               # sizeでない場合はcpu(アイテム)なので、名前とy座標をリストにして辞書に追加
+                        self.dic[key].append([line[0], int(line[1])])
 
     def createCpu(self):
-        if self.x in self.dic:
-            x = self.x + WIDTH
-            for cpu in self.dic[self.x]:
-                name = cpu[0]
-                y = int(cpu[1])
-                self.createOneCpu(name, x, y)
+        """現在のステージ位置にcpu(アイテム)があるならすべて生成する"""
+        if self.x in self.dic:                  # 辞書にself.xの値がキーになっている要素があるか
+            x = self.x + WIDTH                  # あるとき、生成位置xを設定する
+            for cpu in self.dic[self.x]:        # キーself.xにある要素を取り出す
+                name = cpu[0]                   # 名前を設定
+                y = cpu[1]                      # y座標を設定
+                self.createOneCpu(name, x, y)   # 一つだけ生成
 
     def createOneCpu(self, name, x, y):
+        """nameで指定されるcpu(アイテム)を(x, y)に生成。なお、nameはdefine.pyに定義された定数から選択"""
         if name == CPU1:
             CpuMachine(x, y, self.players)
