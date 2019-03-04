ba9b572dcc107527aee69c29bbc0eabe9616ed62
Tomoya Kawabata <Tomoya@gmail.com>
2019-03-04 21:44:42 +0900

    いろいろ変更

--- cpumachine.py
+++ cpumachine.py
@@ -23,8 +23,7 @@ class cpu(CpuMachine):
     
     def update(self):
         self.rect.move_ip(-2.5,0)
-        x, y = self.rect.midright
-        x -= 90
+        x, y = self.rect.midleft
         if pygame.time.get_ticks() - self.gun_start >= 600:
             super().shoot2(x, y)
             self.gun_start = pygame.time.get_ticks()
@@ -60,4 +59,9 @@ class cpu3(CpuMachine):
     def update(self):
         if 0 <= self.count <= 150:
             self.rect.move_ip(-2.5,0)
-            self.count += 1 
+            self.count += 1
+
+        x, y = self.rect.midleft
+        if pygame.time.get_ticks() - self.gun_start >= 1200:
+            super().shoot2(x, y)
+            self.gun_start = pygame.time.get_ticks() 
