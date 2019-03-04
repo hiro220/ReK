6adcbf5beabcff471f3a07d02ccec9deb2c933e2
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-03-04 23:35:08 +0900

    コメントの追加、不要メソッドの削除

--- item.py
+++ item.py
@@ -4,31 +4,29 @@ import pygame
 from pygame.locals import *
 
 class Item(pygame.sprite.Sprite):
-
-    def __init__(self, x, y, img, player):
+    """アイテム処理の基底となるクラス。継承するクラスは、メソッドeffect(self, machine)を定義する。
+    effectメソッドでは、アイテムを取得した機体machineに対して、そのアイテムに応じた処理を行うよう記述する。"""
+    def __init__(self, x, y, img, machine):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = img
         self.rect = self.image.get_rect()
-        self.player = player
-        self.speed = -1
-        self.rect.move_ip(x, y)
+        self.machine = machine                      # アイテムの効果対象
+        self.speed = -1                             # アイテムの移動速度
+        self.rect.move_ip(x, y)                     # 引数で指定された初期位置に移動させる
 
     def update(self):
-        self.rect.move_ip(self.speed, 0)
-        collide_list = pygame.sprite.spritecollide(self, self.player, False)
-        if collide_list:
+        self.rect.move_ip(self.speed, 0)            # 移動
+        collide_list = pygame.sprite.spritecollide(self, self.machine, False)   # 当たり判定
+        if collide_list:                            # アイテムを取得した機体があるなら
             self.kill()
-            for player in collide_list:
-                self.effect(player)
-
-    def effect(self, player):
-        pass
+            for machine in collide_list:
+                self.effect(machine)                # アイテムを取得した機体に対して、アイテムに応じた効果を与える
 
 class Recovery(Item):
-
+    """取得した機体の体力を1回復するアイテム"""
     def __init__(self, x, y, machine):
         image = pygame.image.load("img/recovery.png").convert_alpha()
         super().__init__(x, y, image, machine)
 
-    def effect(self, player):
-        player.recover(1)
+    def effect(self, machine):
+        machine.recover(1)                          # 機体の体力を1回復する
