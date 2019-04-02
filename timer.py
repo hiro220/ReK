4c1bd3f550d0c199365160917dfbd0ea64425084
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-04-02 23:47:21 +0900

    コメントの追加

--- timer.py
+++ timer.py
@@ -5,14 +5,16 @@ from pygame.locals import *
 
 class Timer(pygame.sprite.Sprite):
 
-    def __init__(self, time, method):
+    def __init__(self, millisecond, process):
+        """millisecondミリ秒経過後、processを実行する。
+        Timer(2500, sample(1, 2))のように使う。processには実行するときと同じように関数を記述する。
+        """
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.process = method
-        self.time = time
-        self.init_time = pygame.time.get_ticks()
+        self.process = process                                  # 関数processをインスタンス変数に
+        self.time = millisecond                                 # 時間計測
+        self.init_time = pygame.time.get_ticks()                # 作成時の時間を保持
 
     def update(self):
-        print(pygame.time.get_ticks(), self.init_time)
-        if pygame.time.get_ticks() - self.init_time >= self.time:
-            self.process
-            self.kill()
+        if pygame.time.get_ticks() - self.init_time >= self.time:       # このインスタンスが生成されたときからの経過時間が設定した時間より長い
+            self.process                                        # processを実行
+            self.kill()                                         # このスプライトをグループから削除
