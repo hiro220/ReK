51c0341231ac630d7c2b534d93661ef9a330b37f
Ono111 <k_taro.1119@ezweb.ne.jp>
2019-04-16 18:15:43 +0900

    作成中

--- playermachine.py
+++ playermachine.py
@@ -28,6 +28,9 @@ class PlayerMachine(Machine):
         if key == K_x:              # ｘキーが押されたとき弾を発射
             x, y = self.rect.midright
             super().shoot(x, y)
+
+    def reload(self, key):
+        
     
     def isGameOver(self):
         return not self.alive()
