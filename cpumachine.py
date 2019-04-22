d10c21321686b535f954c7830b3404d3b989d9fb
totem <e1c18042@oit.st.ac.jp>
2019-04-22 17:16:56 +0900

    坂本君のコミットテスト

--- cpumachine.py
+++ cpumachine.py
@@ -70,3 +70,5 @@ class cpu3(CpuMachine):
         if pygame.time.get_ticks() - self.gun_start >= 1200:
             super().Tracking_shoot(x, y)
             self.gun_start = pygame.time.get_ticks() 
+
+    """test"""
