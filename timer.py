0fb354fa5be7932a1a905c2b3444332166ea1f61
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-05-08 21:24:01 +0900

    timerで引数を与えられるようにした

--- timer.py
+++ timer.py
@@ -5,16 +5,17 @@ from pygame.locals import *
 
 class Timer(pygame.sprite.Sprite):
 
-    def __init__(self, millisecond, process):
+    def __init__(self, millisecond, process, *args):
         """millisecondミリ秒経過後、processを実行する。
         Timer(2500, sample)のように使う。processには関数名を記述する。
         """
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.process = process                                  # 関数processをインスタンス変数に
         self.time = millisecond                                 # 時間計測
+        self.args = args
         self.init_time = pygame.time.get_ticks()                # 作成時の時間を保持
 
     def update(self):
         if pygame.time.get_ticks() - self.init_time >= self.time:       # このインスタンスが生成されたときからの経過時間が設定した時間より長い
-            self.process()                                      # processを実行
+            self.process(*self.args)                                      # processを実行
             self.kill()                                         # このスプライトをグループから削除
