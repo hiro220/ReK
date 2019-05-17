cdaf22708bb1aafbb0417bb1bd62505120c7d28e
totem <e1c18042@oit.st.ac.jp>
2019-05-17 20:05:09 +0900

    helpの変更

--- help_explain.py
+++ help_explain.py
@@ -10,15 +10,8 @@ class Help_a:
         explain_font = pygame.font.Font("freesansbold.ttf", 25)
         explain_font1 = pygame.font.Font("freesansbold.ttf", 25)
         with open('testa/test.txt', 'r', encoding="utf-8") as fp:   # ファイルを読み取り専用で開く
-            self.dic1 = {}                     # 辞書を定義する
-            self.dic2 = {}
-            self.dic3 = {}
-            self.dic4 = {}
-            self.dic5 = {}
-            self.dic6 = {}
-            self.dic7 = {}
-            self.dic_data = [self.dic1, self.dic2, self.dic3, self.dic4, self.dic5, self.dic6, self.dic7]
-            for (line, data) in zip(fp.readlines(), range(len(self.dic_data))):                 # ファイルを一行ごとに読み取り、変数lineに文字列として格納する
+            self.dic_data = [0,1 , 2, 3, 4, 5, 6]
+            for (line, data) in zip(fp.readlines(), range(7)):                 # ファイルを一行ごとに読み取り、変数lineに文字列として格納する
                 self.dic_data[data] = line.strip('\n')       # 改行コード'\n'を取り除き、タブ区切りでリストに分割する
                 self.dic_data[data] = explain_font.render(self.dic_data[data], True, (255,255,255))
                 
