f048cee9258ec3e6331a295b8d92f8e588b4a559
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-06-18 14:50:31 +0900

    テーブルの作成とデータの挿入

--- database.py
+++ database.py
@@ -4,8 +4,25 @@ import sqlite3
 
 db = 'data/savedata.db'
 
+# データベース
 conn = sqlite3.connect(db)
-conn.cursor()
+# sqliteを操作するカーソルオブジェクトを作成
+cur = conn.cursor()
 
 try:
-    conn.execute('CREATE TABLE')
+    cur.execute('CREATE TABLE ranking(id INTEGER PRIMARY KEY AUTOINCREMENT, score integer)')
+
+except sqlite3.Error as e:
+    print('sqlite3.Error :', e.args[0])
+
+cur.execute('INSERT INTO ranking(score) values(100)')
+cur.execute('INSERT INTO ranking(score) values(3400)')
+cur.execute('INSERT INTO ranking(score) values(200)')
+cur.execute('INSERT INTO ranking(score) values(120)')
+
+# データベースへの変更をコミット
+conn.commit()
+
+print([data for data in cur.execute('SELECT * FROM ranking')])
+# データベースへのコネクションを閉じる
+conn.close()
