01f3410218de2a33bbfc877ed7f68bd9136dc178
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-07-21 15:40:33 +0900

    処理部分の基礎

--- equipment.py
+++ equipment.py
@@ -6,6 +6,8 @@ class equipment:
 
     def __init__(self, data):
         self.data = data
+        self.selected = 0
+        self.guns_num = len(self.data['gun_data'])
     
     def do(self):
         while True:
@@ -15,7 +17,18 @@ class equipment:
                 return request
 
     def process(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return EXIT
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    self.selected += 1
+                elif event.key == K_DOWN:
+                    self.selected -= 1
+                self.selected = (self.selected+self.guns_num) % self.guns_num
+                elif event.key == K_RETURN:
+                    pass
+        return CONTINUE
 
     def draw(self):
         pass
