6f6440c231060f15d8c240345d7df4a13188d1bf
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-07-23 13:09:51 +0900

    修正

--- boss2.py
+++ boss2.py
@@ -8,7 +8,7 @@ import random
 import math
 
 class Stage2_boss(Boss):
-    def __init__(self, hp, x, y, image, players, score, money):
+    def __init__(self, x, y, players, score, money):
         image = pygame.image.load("img/cpu.png").convert_alpha() #イメージ画像をロードする
         super().__init__(10, x, y, image, players, score, money)         #superクラス(Boss)を呼び出す
         self.dx,self.dy = -2, 0
