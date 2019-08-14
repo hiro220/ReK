670db0dbda72d2f3e972c7ea0887e017c7fd4f07
Ono111 <k_taro.1119@ezweb.ne.jp>
2019-08-14 20:24:41 +0900

    銃の値段を自由に変更できます

--- shop.py
+++ shop.py
@@ -14,34 +14,40 @@ class Shop:
         self.gun_num = 1
         self.back_num = 0
 
-        Circle_gun_font = pygame.font.Font("freesansbold.ttf", 45)
-        Circle_gun_place_font = pygame.font.Font("freesansbold.ttf", 45)
-        Reflection_Gun_font = pygame.font.Font("freesansbold.ttf", 45)
-        Reflection_Gun_place_font = pygame.font.Font("freesansbold.ttf", 45)
+        self.gun_value = [1000, 1000, 1000, 1000, 1000, 1000]
 
         back_font = pygame.font.Font("freesansbold.ttf", 55)
-
-        self.Circle_gun_text = Circle_gun_font.render("Circle_Gun", True, (255, 255, 255))
-        self.Circle_gun_place_text = Circle_gun_place_font.render("1000", True, (255, 255, 255))
-        self.Reflection_Gun_text = Reflection_Gun_font.render("Reflection_Gun", True, (255, 255, 255))
-        self.Reflection_Gun_place_text = Reflection_Gun_place_font.render("1500", True, (255, 255, 255))
         self.back_text = back_font.render("Back", True, (255, 255, 255))
 
     def draw(self):
+        
         while True:
-            self.screen.blit(self.Reflection_Gun_text, [210, 150])
-            self.screen.blit(self.Reflection_Gun_place_text, [700, 250])
-            self.screen.blit(self.Circle_gun_text, [210, 250])
-            self.screen.blit(self.Circle_gun_place_text, [700, 150])
+            for i in range(1, 7):
+                gun_id = i
+                gun = self.data['gun_data'][gun_id]
+                gun_text = gun['name']
+
+                if gun['own'] == 0:
+                    color = (255, 255, 255)
+                else:
+                    color = (150, 150, 150)
+                    
+                draw_text = pygame.font.Font("freesansbold.ttf", 45).render(gun_text, True, color)
+                self.screen.blit(draw_text, [210, 50 * i + 110])
+
+                gun_value_text = str(self.gun_value[i - 1])
+                gun_value_font = pygame.font.Font("freesansbold.ttf", 45).render(gun_value_text, True, (255, 255, 255))
+                self.screen.blit(gun_value_font, [700, 50 * i + 110])
+
+                if self.gun_num == gun_id:
+                    width = draw_text.get_rect().right
+                    height = draw_text.get_rect().bottom
+                    pygame.draw.rect(self.screen,(255,255,0),Rect(205, 105 + i * 50, width+10, height+10),5)
+
             self.screen.blit(self.back_text, [900, 5])
             
             own_money = pygame.font.SysFont(None, 55).render("MONEY:" + str(self.data['money']), True, (255, 255, 255))
             self.screen.blit(own_money, (10, 10))
-
-            if self.gun_num == 1:
-                pygame.draw.rect(self.screen,(255,255,0),Rect(210,150,350,60),5)
-            elif self.gun_num == 2:
-                pygame.draw.rect(self.screen,(255,255,0),Rect(210,250,250,60),5)
             
             if self.back_num == 1:
                 pygame.draw.rect(self.screen,(255,255,0),Rect(890,3,150,60),5)
@@ -65,20 +71,20 @@ class Shop:
                 self.back_num = 0
                 self.gun_num = 1
             else:
-                if self.gun_num >= 1 and self.gun_num < 2:
+                if self.gun_num >= 1 and self.gun_num < 6:
                     self.gun_num += 1
         elif event.key == K_UP:
             if self.gun_num == 1:
                 self.back_num = 1
                 self.gun_num = 0
             else:
-                if self.gun_num > 1 and self.gun_num <= 2:
+                if self.gun_num > 1 and self.gun_num <= 6:
                     self.gun_num -= 1
 
 
     def Buy(self):
         data = self.data['gun_data']
         if data[self.gun_num]['own'] == 0:
-            if self.data['money'] >= 1000:
-                self.data['money'] -= 1000
+            if self.data['money'] >= self.gun_value[self.gun_num - 1]:
+                self.data['money'] -= self.gun_value[self.gun_num - 1]
                 data[self.gun_num]['own'] = 1
