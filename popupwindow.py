ddb3070bbea78aa00266cbf7f1f9083b3b35119d
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-08-20 19:37:28 +0900

    キー入力によるボタン移動、選択

--- popupwindow.py
+++ popupwindow.py
@@ -7,18 +7,29 @@ class PopupWindow:
         self.screen = screen
         self.text = text
         self.buttons = buttons
+        self.button_id = 0
+        self.button_size = len(buttons)
         self.clock = pygame.time.Clock()
 
     def loop(self):
         while True:
             self.clock.tick(30)
-            button_id = self.process()
+            request = self.process()
             self.draw()
-            if button_id != None:
-                return button_id
+            if request != None:
+                return self.button_id
 
     def draw(self):
         pass
 
     def process(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_RIGHT:
+                    self.button_id += 1
+                elif event.key == K_LEFT:
+                    self.button_id -= 1
+                elif event.key == K_RETURN:
+                    return 1
+                self.button_id = (self.button_id + self.button_size) % self.button_size
+        return None
