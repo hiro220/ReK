f9ffb03b5f937f6190c795e4c4d552989bc9e23b
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-08-24 16:37:52 +0900

    ボタンおよびボタン内のテキスト描画

--- popupwindow.py
+++ popupwindow.py
@@ -8,11 +8,20 @@ class PopupWindow:
     def __init__(self, screen, text="", buttons=[]):
         self.screen = screen
         self.text = text
-        self.buttons = buttons
-        self.button_id = 0
-        self.button_size = len(buttons) or 1
+        center_x, center_y = WIDTH // 2, HEIGHT // 2
+        self.rect = Rect(center_x-250, center_y-150, 500, 300)
         x, y = WIDTH // 2 - 230, HEIGHT // 2 - 140
         self.textbox = TextBox(screen, x, y, 460, 200, text, outline_color=(255,255,255), font_size=25)
+        self.buttons = []
+        self.button_id = 0
+        self.button_size = len(buttons) or 1
+        frame = 500 / self.button_size
+        x = self.rect.left + frame/2 - 50
+        y = self.rect.bottom - 80
+        for button in buttons:
+            button = TextBox(screen, x, y, 100, 50, button, outline_color=(255,255,255))
+            x += frame
+            self.buttons.append(button)
         self.clock = pygame.time.Clock()
 
     def loop(self):
@@ -24,16 +33,12 @@ class PopupWindow:
                 return self.button_id
 
     def draw(self):
-        center_x, center_y = WIDTH // 2, HEIGHT // 2
-        box = Rect(center_x-250, center_y-150, 500, 300)
-        pygame.draw.rect(self.screen, (255,255,255), box)
-        pygame.draw.rect(self.screen, (150,150,150), box, 5)
+        pygame.draw.rect(self.screen, (255,255,255), self.rect)
+        pygame.draw.rect(self.screen, (150,150,150), self.rect, 5)
         self.textbox.draw()
-        frame = 500 / self.button_size
-        x = box.left + frame/2 - 50
-        for button_text in self.buttons:
-            pygame.draw.rect(self.screen, (0,0,0), Rect(x, box.bottom-80, 100, 50), 3)
-            x += frame
+        for button in self.buttons:
+            button.draw()
+            pygame.draw.rect(self.screen, (0,0,0), button.rect, 3)
         pygame.display.update()
 
     def process(self):
