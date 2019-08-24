47bdf98bb8f6b1503617bf0d2bb2c925f6bee866
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-08-24 17:29:51 +0900

    テキストの表示位置の整列が引数で指定できるようにした

--- textbox.py
+++ textbox.py
@@ -4,7 +4,7 @@ import unicodedata
 
 class TextBox:
     def __init__(self, screen, x, y, width, height, text='', bg=(255,255,255), text_color=(0,0,0), font_size=20, \
-                 outline_color=(150,150,150), full_font="", half_font='freesansbold.ttf'):
+                 outline_color=(150,150,150), full_font="", half_font='freesansbold.ttf', align=('left', 'top')):
         self.screen = screen
         self.rect = Rect(x, y, width, height)
         self.bg = bg
@@ -13,21 +13,38 @@ class TextBox:
         self.outline_color = outline_color
         texts = self.separate_text(text)
         self.texts = self.create_text(texts, full_font, half_font)
+        assert align[0] in ('left', 'center', 'right'), "選択できる横向きのalign属性('left', 'center', 'right')"
+        assert align[1] in ('top', 'center', 'bottom'), "選択できる縦向きのalign属性('top', 'center', 'buttom')"
+        self.align = align
 
     def draw(self):
         # 描画領域の塗りつぶし
         pygame.draw.rect(self.screen, self.bg, self.rect)
         # 枠線の描画
         pygame.draw.rect(self.screen, self.outline_color, self.rect, 3)
+        height = len(self.texts) * self.font_size
         for i, oneline in enumerate(self.texts):
+            width = oneline[-1]
             # テキストの表示位置
-            x, y = self.rect.left+5, self.rect.top + 5 + self.font_size * i
+            x = self.rect.left + 5
+            y = self.rect.top + self.font_size * i + 3
+            frame_width, frame_height = self.rect.right - self.rect.left - 10, self.rect.bottom - self.rect.top - 6
+            # 横方向に対する整列
+            if self.align[0] == 'center':
+                x += (frame_width - width) / 2
+            elif self.align[0] == 'right':
+                x += frame_width - width
+            # 縦方向に対する整列
+            if self.align[1] == 'center':
+                y += (frame_height - height) / 2
+            elif self.align[1] == 'bottom':
+                y += frame_height - height
             # 一行表示
-            for text in oneline:
+            for text in oneline[:-1]:
                 # 全角と半角で分離されているので、すべて描画
                 rect = text.get_rect()
                 self.screen.blit(text, [x, y])
-                x += rect.right - rect.left
+                x += rect.right
 
     def separate_text(self, text):
         """入力したテキストを、全角部分と半角部分で分割し、順番はそのままにリストにする。"""
@@ -64,15 +81,18 @@ class TextBox:
                 char = font.render(char, True, self.text_color)
                 size = char.get_rect().right
                 width += size
-                # 描画枠を出るようなら改行
                 if width <= frame_width:
+                    # 枠内に収まる
                     one_line.append(char)
                 else:
+                    # 枠内に収まらない
                     # 一行分をリストに格納し、次の行を作成
+                    one_line.append(width-size)
                     width = size
                     font_texts.append(one_line)
                     one_line = []
                     one_line.append(char)
+        one_line.append(width)
         font_texts.append(one_line)
         return font_texts
 
