299641b1ba6fd1041bb8efba581c6e75927279fa
hiroki-inoue-git <weeklyjump0928@gmail.com>
2019-09-26 22:51:34 +0900

    printデバッグ削除

--- stage.py
+++ stage.py
@@ -80,7 +80,6 @@ class Stage:
         while True:
             self.clock.tick(30)         # フレームレート(30fps)
             result = self.process()
-            print(self.bullets)
             self.draw()
             pygame.display.update()     # 画面更新
             if not result == CONTINUE:
